0fb8a307ef8eae8c7c787192b462d43d7aff3771
Waldyn Benbenek <wben1201@gmail.com>
2019-02-08 15:56:06 -0600

    initialized and installed dependencies.

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
-
+let answer = 0
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
